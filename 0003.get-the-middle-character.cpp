8aae3256ff4d4981f7368e9583cf60ae1760b9be
Irina Hristova <hristova119@gmail.com>
2019-11-23 19:35:34 +0200

    feat: 0003. sln

---
+++ 0003.get-the-middle-character.cpp
@@ -0,0 +1,15 @@
+std::string get_middle(std::string input)
+{
+	std::string result = "";
+	int len = input.size();
+	if (len % 2 == 1)
+	{
+		result = input[len / 2];
+	}
+	else
+	{
+		result = input.substr(len / 2 - 1, 2);
+	}
+
+	return result;
+}
