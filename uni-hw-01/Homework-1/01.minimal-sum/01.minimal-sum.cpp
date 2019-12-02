2148c0f37518fdb44fcca63ae90dec1870ec11c5
Irina Hristova <hristova119@gmail.com>
2019-12-02 02:41:35 +0200

    chore: add files

--- uni-hw-01/Homework-1/01.minimal-sum/01.minimal-sum.cpp
+++ uni-hw-01/Homework-1/01.minimal-sum/01.minimal-sum.cpp
@@ -2,5 +2,26 @@
 
 int main()
 {
+	int n;
+
+	std::cin >> n;
+	int i;
+
+	// minimal sum = sum of the prime factors 
+	for (i = 2; i * i <= n; i++)
+	{
+		while (n % i == 0)
+		{
+			std::cout << i << " " << std::endl;
+
+			n /= i;
+		}
+	}
+
+	if (n > 1)
+	{
+		std::cout << n << " " << std::endl;
+	}
+
 	return 0;
 }
--- uni-hw-01/Homework-1/03.numbers/03.numbers.cpp
+++ uni-hw-01/Homework-1/03.numbers/03.numbers.cpp
@@ -1,6 +1,57 @@
 #include <iostream>
+using namespace std;
 
 int main()
 {
+	// generate all numbers with the digits given 
+	// check whether the number is in the interval
+	// print in ascending number
+
+	int a, b, n, num;
+	int digits[10];
+	bool flag = true;
+	cin >> a >> b;
+	cin >> n;
+
+	for (int i = 0; i < n; i++)
+	{
+		cin >> digits[i];
+	}
+
+	for (int i = a; i <= b; i++)
+	{
+		flag = true;
+		num = i;
+		while (num > 0)
+		{
+			int rem = num % 10;
+			for (int j = 0; j < n; j++)
+			{
+				if (rem == digits[j] && i < 10)
+				{
+					flag = true;
+					break;
+				}
+				if (rem != digits[j] && i >= 10)
+				{
+
+				}
+			}
+
+			if (!flag)
+			{
+				break;
+			}
+
+			num = num / 10;
+		}
+
+		if (flag)
+		{
+			cout << i << " ";
+		}
+	}
+
+
 	return 0;
 }
