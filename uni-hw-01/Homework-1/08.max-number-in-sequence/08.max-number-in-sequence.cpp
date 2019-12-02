d8181ec28c779d2bd6027ec98497e2634e93b485
Irina Hristova <hristova119@gmail.com>
2019-12-02 02:39:38 +0200

    feat: cpp sln 08

--- uni-hw-01/Homework-1/08.max-number-in-sequence/08.max-number-in-sequence.cpp
+++ uni-hw-01/Homework-1/08.max-number-in-sequence/08.max-number-in-sequence.cpp
@@ -2,5 +2,35 @@
 
 int main()
 {
+	const int MaxLength = 1001;
+	char arr[MaxLength];
+	int number = 0, max = 0;
+
+	std::cin.getline(arr, MaxLength);
+
+	for (int i = 0; i < MaxLength; i++)
+	{
+		if (arr[i] == '\0')
+		{
+			break;
+		}
+		
+		if (arr[i] >= '0' && arr[i] <= '9')
+		{
+			if (number != 0)
+			{
+				number *= 10;
+			}
+			number += (arr[i] - '0');
+		}
+		else
+		{
+			max = max < number ? number : max;
+			number = 0;
+		}
+	}
+
+	std::cout << max << std::endl;
+
 	return 0;
 }
