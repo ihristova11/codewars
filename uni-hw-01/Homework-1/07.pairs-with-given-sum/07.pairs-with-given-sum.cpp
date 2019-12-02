89dd9fcfe064e673894966dac3fdcb9495e67f20
Irina Hristova <hristova119@gmail.com>
2019-12-02 02:13:34 +0200

    feat: cpp sln 07

--- uni-hw-01/Homework-1/07.pairs-with-given-sum/07.pairs-with-given-sum.cpp
+++ uni-hw-01/Homework-1/07.pairs-with-given-sum/07.pairs-with-given-sum.cpp
@@ -1,6 +1,40 @@
 #include <iostream>
 
+int findPairsWithSum(int arr[], int n, int sum)
+{
+	int count = 0;
+
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = i + 1; j < n; j++)
+		{
+			if (arr[i] + arr[j] == sum)
+			{
+				count++;
+			}
+		}
+	}
+
+	return count;
+}
+
 int main()
 {
+	const int MaxLength = 200;
+	int n, sum, pairs = 0;
+	int seq[MaxLength];
+
+	std::cin >> n;
+
+	for (int i = 0; i < n; i++)
+	{
+		std::cin >> seq[i];
+	}
+
+	std::cin >> sum;
+
+	pairs = findPairsWithSum(seq, n, sum);
+	std::cout << pairs << std::endl;
+
 	return 0;
 }
