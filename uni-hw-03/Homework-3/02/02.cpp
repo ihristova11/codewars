ee1c5a6171b9c1629ac7d4b913eeaa3d1e820651
Irina Hristova <hristova119@gmail.com>
2020-01-12 16:05:22 +0200

    refactor:

--- uni-hw-03/Homework-3/02/02.cpp
+++ uni-hw-03/Homework-3/02/02.cpp
@@ -1,5 +1,10 @@
 #include <iostream>
 
+// implementation idea: 
+// turn the n-1 lamp so that the last one can be turned on
+// turn on the last n-th lamp in the line
+// turn on the remaining lamps
+
 // should be declared because we use them 
 // in first function implementation
 void turnOnAll(int n);
@@ -10,16 +15,14 @@ void turnFirstOn(int current)
 {
 	if (current == 1)
 	{
-		std::cout << "+1" << std::endl;
-	}
-	else
-	{
-		// recurrence call to turn on the next lamp
-		turnFirstOn(current - 1);
-
 		std::cout << "+" << current << std::endl;
-		turnOffAll(current - 1);
+		return;
 	}
+	// recurrence call to turn on the next lamp
+	turnFirstOn(current - 1);
+
+	std::cout << "+" << current << std::endl;
+	turnOffAll(current - 1);
 }
 
 // turns on all lamps in the line
@@ -28,19 +31,21 @@ void turnOnAll(int n)
 	if (n == 1)
 	{
 		std::cout << "+" << n << std::endl;
+		return;
 	}
-	else if (n == 2)
-	{
-		std::cout << "+1" << std::endl << "+2" << std::endl;
-	}
-	else
-	{
-		turnFirstOn(n - 1);
-		std::cout << "+" << n << std::endl;
 
-		// turn on all the other lamps in the line reccurently
-		turnOnAll(n - 2);
+	if (n == 2)
+	{
+		std::cout << "+" << n - 1 << std::endl 
+			<< "+" << n << std::endl;
+		return;
 	}
+
+	turnFirstOn(n - 1);
+	std::cout << "+" << n << std::endl;
+
+	// turn on all the other lamps in the line reccurently
+	turnOnAll(n - 2);
 }
 
 // turns off all the lamps in the line
@@ -49,15 +54,13 @@ void turnOffAll(int n)
 	if (n == 1)
 	{
 		std::cout << "-" << n << std::endl;
+		return;
 	}
-	else
-	{
-		turnFirstOn(n - 1);
-		std::cout << "-" << n << std::endl;
+	turnFirstOn(n - 1);
+	std::cout << "-" << n << std::endl;
 
-		// turn off all the other lamps in the line recurrently
-		turnOffAll(n - 1);
-	}
+	// turn off all the other lamps in the line recurrently
+	turnOffAll(n - 1);
 }
 
 int main()
